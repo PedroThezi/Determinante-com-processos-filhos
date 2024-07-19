3f5e2af33461619e1f11905922601fe72dbd3b36
Pedro Thezi <pedro.maschieto130@gmail.com>
2024-07-19 11:36:03 -0300

    processos

---
+++ Pipe/pipe.c
@@ -0,0 +1,96 @@
+//Pedro Antonio Maschieto Thezi
+//Daniel Venturini Pereira
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int matriz[3][3];
+int fd[2];
+int result1,result2;
+
+void filho1()
+{
+    close(fd[0]);
+    printf("\nFilho1: calculando\n");
+    result1 = matriz[0][0] * matriz[1][1] * matriz[2][2] +
+                  matriz[0][1] * matriz[1][2] * matriz[2][0] +
+                  matriz[0][2] * matriz[1][0] * matriz[2][1];
+    printf("Filho1: resultado: %d\n",result1);
+    write(fd[1], &result1, sizeof(result1));
+    close(fd[1]);
+    exit(0);
+}
+
+void filho2()
+{
+    close(fd[0]);
+    printf("\nFilho2: calculando\n");
+    result2 = (matriz[0][2] * matriz[1][1] * matriz[2][0] +
+                   matriz[0][0] * matriz[1][2] * matriz[2][1] +
+                   matriz[0][1] * matriz[1][0] * matriz[2][2]) * -1;
+    printf("Filho2: resultado: %d\n",result2);
+    write(fd[1], &result2, sizeof(result2));
+    close(fd[1]);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *file = fopen("matriz.txt", "r");
+    char line[20];
+    fgets(line, sizeof(line), file);
+    char *token = strtok(line, " ");
+
+    int i, j;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            matriz[i][j] = atoi(token);
+            token = strtok(NULL, " ");
+        }
+    }
+    fclose(file);
+
+    printf("Matriz: ");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n\t");
+    }
+
+    pipe(fd);
+    pid_t filho1_pid, filho2_pid;
+
+    if ((filho1_pid = fork()) == 0)
+    {
+        filho1();
+    }
+    else if ((filho2_pid = fork()) == 0)
+    {
+        filho2();
+    }
+    else
+    {
+
+        close(fd[1]);
+
+	waitpid(filho1_pid, NULL, 0);
+      	read(fd[0], &result1, sizeof(result1));
+	
+	waitpid(filho2_pid, NULL, 0);
+        read(fd[0], &result2, sizeof(result2));
+
+        close(fd[0]);
+
+        int determinante = result1 + result2;
+        printf("\nO determinante da matriz: %d\n", determinante);
+
+    }
+    return 0;
+}
---
+++ Thread/thread.c
@@ -0,0 +1,74 @@
+//Pedro Antonio Maschieto Thezi RA:202310236
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+int matriz[3][3];
+int result = 0;
+pthread_mutex_t mutex;
+
+void *filho1(void *arg) {
+    printf("\nFilho1:Calculando...\n");
+    int result1 = matriz[0][0] * matriz[1][1] * matriz[2][2] +
+                 matriz[0][1] * matriz[1][2] * matriz[2][0] +
+                 matriz[0][2] * matriz[1][0] * matriz[2][1];
+
+    pthread_mutex_lock(&mutex);
+    result += result1;
+    pthread_mutex_unlock(&mutex);
+    printf("Resultado do filho1: %d\n",result1);
+    printf("Filho1:saindo...\n");
+}
+
+void *filho2(void *arg) {
+    printf("\nFilho2:Calculando...\n");
+    int result2 = (matriz[0][2] * matriz[1][1] * matriz[2][0] +
+                  matriz[0][0] * matriz[1][2] * matriz[2][1] +
+                  matriz[0][1] * matriz[1][0] * matriz[2][2]) * -1;
+
+    pthread_mutex_lock(&mutex);
+    result += result2;
+    pthread_mutex_unlock(&mutex);
+    printf("Resultado do filho2: %d\n",result2);
+    printf("Filho2:saindo...\n");
+}
+
+int main(int argc, char *argv[]) {
+    FILE *file = fopen("matriz.txt", "r");
+    char line[20];
+    fgets(line, sizeof(line), file);
+    char *token = strtok(line, " ");
+
+    int i, j;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            matriz[i][j] = atoi(token);
+            token = strtok(NULL, " ");
+        }
+    }
+
+    fclose(file);
+    printf("matriz: ");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ",matriz[i][j]);
+        }
+        printf("\n\t");
+    }
+
+    pthread_t thread_id[2];
+    pthread_mutex_init(&mutex, NULL);
+
+    pthread_create(&thread_id[0], NULL, filho1, NULL);
+    pthread_create(&thread_id[1], NULL, filho2, NULL);
+
+    pthread_join(thread_id[0], NULL);
+    pthread_join(thread_id[1], NULL);
+
+    printf("\nO determinante da matriz eh: %d\n", result);
+
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
+}
